b08b08d9bb9d0e081052a44c03424f221d5d195b
Michael Meuli <mmeuli@imm.uzh.ch>
2024-12-16 16:05:35 +0100

    implement views

--- src/main.rs
+++ src/main.rs
@@ -195,60 +195,8 @@ impl Tbgui {
             }) => match screen {
                 //Screen::Home => self.view_home(),
                 //Screen::Settings => self.view_settings(),
-                Screen::Home => {
-                    let title = text("TB-Profiler")
-                    .width(Fill)
-                    .size(60)
-                    .color([0.5, 0.5, 0.5])
-                    .align_x(Center);
-
-                //let button = button("Run Profiler").on_press(Message::RunTbProfiler);
-                //let button: Element<Message> = button("Run Profiler").into();
-                let run_controls = row![
-                    button("Run Profiler").on_press(Message::RunTbProfiler),
-                    button("Download Results").on_press(Message::DownloadResults),
-                    button("Delete Results").on_press(Message::DeleteResults),
-                    gear_button(),
-                ]
-                .spacing(20);
-                let controls = view_controls(items, *filter);
-                let filtered_items = items.iter().filter(|item| filter.matches(item));
-
-                let items: Element<_> = if filtered_items.count() > 0 {
-                    keyed_column(
-                        items
-                            .iter()
-                            .enumerate()
-                            .filter(|(_, item)| filter.matches(item))
-                            .map(|(i, item)| {
-                                (
-                                    item.id,
-                                    item.view()
-                                        .map(move |message| Message::ItemMessage(i, message)),
-                                )
-                            }),
-                    )
-                    .spacing(10)
-                    .into()
-                } else {
-                    if let Some(error) = error_message {
-                        empty_message(error)
-                    } else {
-                        empty_message(match filter {
-                            Filter::All => "No raw read sequences found...",
-                            Filter::Unchecked => "All raw read sequences selected...",
-                            Filter::Checked => "No raw read sequences selected...",
-                        })
-                    }
-                };
-
-                let content = column![title, run_controls, controls, items]
-                    .spacing(20)
-                    .max_width(800);
-
-                scrollable(container(content).center_x(Fill).padding(40)).into()
-
-                }
+                Screen::Home => Tbgui::view_home(filter, items, error_message),
+     
                 Screen::Settings => {
                     let title = text("Settings")
                         .width(Fill)
@@ -291,6 +239,61 @@ impl Tbgui {
         //let periodic_subscription = time::every(Duration::from_secs(9 * 60)).map(|_| Message::DownloadResults);
         //Subscription::batch(vec![keyboard_subscription, periodic_subscription])
     }
+
+
+    fn view_home<'a>(
+        filter: &'a Filter,
+        items: &'a [Item],
+        error_message: &'a Option<String>,
+    ) -> Element<'a, Message> {
+        let title = text("TB-Profiler")
+            .width(Fill)
+            .size(60)
+            .color([0.5, 0.5, 0.5])
+            .align_x(Center);
+
+        let run_controls = row![
+            button("Run Profiler").on_press(Message::RunTbProfiler),
+            button("Download Results").on_press(Message::DownloadResults),
+            button("Delete Results").on_press(Message::DeleteResults),
+            gear_button(),
+        ]
+        .spacing(20);
+
+        let controls = view_controls(items, *filter);
+        let filtered_items = items.iter().filter(|item| filter.matches(item));
+
+        let items: Element<_> = if filtered_items.count() > 0 {
+            keyed_column(
+                items
+                    .iter()
+                    .enumerate()
+                    .filter(|(_, item)| filter.matches(item))
+                    .map(|(i, item)| {
+                        (
+                            item.id,
+                            item.view()
+                                .map(move |message| Message::ItemMessage(i, message)),
+                        )
+                    }),
+            )
+            .spacing(10)
+            .into()
+        } else {
+            if let Some(error) = error_message {
+                empty_message(error)
+            } else {
+                empty_message(match filter {
+                    Filter::All => "No raw read sequences found...",
+                    Filter::Unchecked => "All raw read sequences selected...",
+                    Filter::Checked => "No raw read sequences selected...",
+                })
+            }
+        };
+
+        scrollable(container(column![title, run_controls, controls, items].spacing(20).max_width(800)).center_x(Fill).padding(40)).into()
+    }
+
 }
 
 
