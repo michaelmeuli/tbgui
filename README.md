ca448547078021a10d90c0b7ed791aac97268746
Michael Meuli <mmeuli@imm.uzh.ch>
2025-01-31 20:46:40 +0100

    take environment variables as defaults if set

--- README.md
+++ README.md
@@ -4,7 +4,14 @@ A simple GUI front-end for [TB-Profiler](https://github.com/jodyphelan/TBProfile
 
 For now only works with [AuthMethod::with_key_file(key_path, None)](https://docs.rs/async-ssh2-tokio/latest/async_ssh2_tokio/client/enum.AuthMethod.html#method.with_key_file).<br>
 This private key file has to be in [directories_next::UserDirs::home_dir](https://docs.rs/directories-next/latest/directories_next/struct.UserDirs.html#method.home_dir): e.g.: C:\Users\Alice\.ssh\id_rsa<br>
-The scripts executed on the remote cluster can be found here: [tbgui-server-scripts](https://github.com/michaelmeuli/tbgui-server-scripts).<br>
+The scripts executed on the remote cluster can be found here: [tbgui-server-scripts](https://github.com/michaelmeuli/tbgui-server-scripts).<br><br>
+Uses environment variables as defaults if set:
+- TBGUI_USERNAME
+- REMOTE_RAW_DIR
+- TB_PROFILER_SCRIPT
+- REMOTE_RESULTS_DIR
+- DEFAULT_TEMPLATE_REMOTE
+- USER_TEMPLATE_REMOTE <br>
 
 ## cargo doc
 
--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,12 @@
 use serde::{Deserialize, Serialize};
 
+pub const TBGUI_USERNAME: &str = default_env(option_env!("TBGUI_USERNAME"), "mimeul");
+pub const REMOTE_RAW_DIR: &str = default_env(option_env!("REMOTE_RAW_DIR"), "/shares/sander.imm.uzh/MM/PRJEB57919/raw");
+pub const TB_PROFILER_SCRIPT: &str = default_env(option_env!("TB_PROFILER_SCRIPT"), "/shares/sander.imm.uzh/MM/PRJEB57919/scripts/tbprofiler.sh");
+pub const REMOTE_RESULTS_DIR: &str = default_env(option_env!("REMOTE_RESULTS_DIR"), "/shares/sander.imm.uzh/MM/PRJEB57919/out/results");
+pub const DEFAULT_TEMPLATE_REMOTE: &str = default_env(option_env!("DEFAULT_TEMPLATE_REMOTE"), "/shares/sander.imm.uzh/MM/PRJEB57919/tb-profiler-templates/docx/default_template.docx");
+pub const USER_TEMPLATE_REMOTE: &str = default_env(option_env!("USER_TEMPLATE_REMOTE"), "/shares/sander.imm.uzh/MM/PRJEB57919/template/user_template.docx");
+
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct TbguiConfig {
     pub username: String,
@@ -13,12 +20,19 @@ pub struct TbguiConfig {
 impl ::std::default::Default for TbguiConfig {
     fn default() -> Self {
         Self {
-            username: "mimeul".into(),
-            remote_raw_dir: "/shares/sander.imm.uzh/MM/PRJEB57919/raw".into(),
-            tb_profiler_script: "/shares/sander.imm.uzh/MM/PRJEB57919/scripts/tbprofiler.sh".into(),
-            remote_results_dir: "/shares/sander.imm.uzh/MM/PRJEB57919/out/results".into(),
-            default_template_remote: "/shares/sander.imm.uzh/MM/PRJEB57919/tb-profiler-templates/docx/default_template.docx".into(),
-            user_template_remote: "/shares/sander.imm.uzh/MM/PRJEB57919/template/user_template.docx".into(),
+            username: TBGUI_USERNAME.into(),
+            remote_raw_dir: REMOTE_RAW_DIR.into(),
+            tb_profiler_script: TB_PROFILER_SCRIPT.into(),
+            remote_results_dir: REMOTE_RESULTS_DIR.into(),
+            default_template_remote: DEFAULT_TEMPLATE_REMOTE.into(),
+            user_template_remote: USER_TEMPLATE_REMOTE.into(),
         }
+    }
+}
+
+const fn default_env(v: Option<&'static str>, default: &'static str) -> &'static str {
+    match v {
+        Some(v) => v,
+        None => default,
     }
 }
